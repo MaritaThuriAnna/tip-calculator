935e5e1ceb4370d74ec063c9b23811c9210dbd1f
Marita-Thuri Anna <maritathurianna@gmail.com>
2024-10-02 12:57:47 +0300

    percent calculator done

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const amountInput = document.getElementById('amount');
 const tipButtons = document.querySelectorAll('.tip-btn');
 
+
+function calcTipAmount(billTotal, tipPercentage){
+    if(!isNaN(billTotal) && billTotal > 0 && !isNaN(tipPercentage)){
+        const tipAmount = billTotal * (tipPercentage / 100).toFixed(2);
+        console.log("Tip Amount: " + tipAmount);
+    }else {
+        console.log("Please enter a valid bill amount and select a valid tip percentage.");
+    }
+}
+
 tipButtons.forEach(button => {
     button.addEventListener('click', function(event) {
         const tipPercentage = parseInt(event.target.getAttribute('data-percent'));
@@ -8,6 +18,7 @@ tipButtons.forEach(button => {
 
         if (!isNaN(billTotal) && billTotal > 0) {
             console.log("Entered bill total: " + billTotal.toFixed(2));
+            calcTipAmount(billTotal, tipPercentage);
         } else {
             console.log("Please enter a valid bill amount.");
         }
